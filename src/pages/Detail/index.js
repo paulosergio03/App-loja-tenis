dd7ae7689a1e9fa561e1fe02739dbfa973472e0f
Paulo Sérgio <paulosergioadm9@gmail.com>
2023-12-12 19:52:48 -0300

    Adicionado Detalhes

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,47 +1,88 @@
-import React from 'react';
-import { View, Text, Image, ScrollView, TouchableOpacity, StyleSheet } from 'react-native';
+import React from "react";
+import {
+  View,
+  Text,
+  Image,
+  ScrollView,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
 
-export default function Detail({ navigation }){
+import Dot from "../../assets/component/Shoes/Dot";
 
+import SizeButton from "../../assets/component/SizeButton";
+
+export default function Detail({ navigation }) {
   navigation.setOptions({
-    headerTitle: 'Nike Downshifter 10'
-  })
+    headerTitle: 'Nike Downshifter 10',
+  });
 
- return (
-   <ScrollView styles={styles.container}>
-    <Image
-    source={require('../../assets/detail.png')}
-    style={styles.image}
-    resizeMode="cover"
-    />
-     <View>
-         <View>
-           <Text style={[styles.title, { fontSize: 24 } ]}>R$ 280,90</Text>
-         </View>
-         <View opacity={0.4}>
-           <Text style={[styles.title, { fontSize: 30 } ]}>Nike Downshifter 10</Text>
-         </View>
+  return (
+    <ScrollView styles={styles.container}>
+      <Image
+        source={require("../../assets/detail.png")}
+        style={styles.image}
+        resizeMode="cover"
+      />
+      <View>
+        <View>
+          <Text style={[styles.title, { fontSize: 24 }]}>R$ 280,90</Text>
+        </View>
+        <View opacity={0.4}>
+          <Text style={[styles.title, { fontSize: 30 }]}>
+            Nike Downshifter 10
+          </Text>
+        </View>
+        {/* Opções de cores do tênis*/}
+        <View style={styles.dotContainer}>
+          <Dot color="#2379f4" />
+          <Dot color="#fb6e53" />
+          <Dot color="#ddd" />
+          <Dot color="#000" />
+        </View>
 
-         </View>
-    
-
-   </ScrollView>
+        <View style={{ flexDirection: "row", width: "100%" }}>
+          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+            <SizeButton bgColor="#17181a" color="#FFF">
+              40
+            </SizeButton>
+            <SizeButton>37</SizeButton>
+            <SizeButton>44</SizeButton>
+            <SizeButton>42</SizeButton>
+          </ScrollView>
+        </View>
+        <View style={styles.textContent}>
+          <Text style={styles.textTitle}>
+            Nike Downshifter 10
+          </Text>
+          <Text style={styles.textContent}> O Tênis Nike Masculino Downshifter 10 traz amortecimento e suporte atualizados, para garantir uma corrida estável e confortável. Esse tênis de corrida é confeccionado em material respirável, cabedal em couro sintético</Text>
+          <Text>
+            - Categoria: Amortecimento
+          </Text>
+          <Text>
+            - Material: Mesh
+          </Text>
+        </View>
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-   container:{
-    flex:1,
-    width: '100%',
-    backgroundColor: '#FFF'
-
+  container: {
+    flex: 1,
+    width: "100%",
+    backgroundColor: "#FFF",
   },
-  image:{
-    width: '100%'
+  image: {
+    width: "100%",
   },
-  title:{
-    fontFamily:'Anton_400Regular',
-    paddingHorizontal:'2%'
-  }
-
+  title: {
+    fontFamily: "Anton_400Regular",
+    paddingHorizontal: "2%",
+  },
+  dotContainer: {
+    flexDirection: "row",
+    marginVertical: "7%",
+  },
 });
